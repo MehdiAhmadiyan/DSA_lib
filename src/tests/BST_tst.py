4e301b06478afda0c2891f347b8dbbdf834d31bb
Sina Moradi <sinamoradi002@gmail.com>
2026-02-14 12:16:13 +0330

    BST tests complete Phase 1 tests complete

--- src/tests/BST_tst.py
+++ src/tests/BST_tst.py
@@ -18,6 +18,90 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.tree.size, 2)
         self.insert(6)
         self.assertEqual(self.tree.root.right.left.data, 6)
+    def test_is_empty(self):
+        self.assertTrue(self.tree.is_empty())
+        self.tree.insert(5)
+        self.assertFalse(self.tree.is_empty())
+    def test_size(self):
+        self.assertEqual(self.tree.size(), 0)
+        self.tree.insert(6)
+        self.assertEqual(self.tree.size(), 1)
+
+    def test_hight(self):
+        self.assertEqual(self.tree.hight(), 0)
+        self.tree.insert(5)
+        self.assertEqual(self.tree.hight(), 1)
+        self.insert(6)
+        self.insert(4)
+        self.insert(3)
+        self.assertEqual(self.tree.hight(), 2)
+
+    def test_preorder(self):
+        self.assertIsNone(self.tree.preorder())
+        self.tree.insert(5)
+        self.tree.insert(4)
+        self.tree.insert(9)
+        self.tree.insert(7)
+        self.tree.insert(2)
+        self.tree.insert(3)
+        self.assertEqual(self.tree.preorder(), [5, 4, 2, 3, 9, 7])
+
+    def test_inorder(self):
+        self.assertIsNone(self.tree.inorder())
+        self.tree.insert(5)
+        self.tree.insert(4)
+        self.tree.insert(9)
+        self.tree.insert(7)
+        self.tree.insert(2)
+        self.tree.insert(3)
+        self.assertEqual(self.tree.inorder(), [2, 3, 4, 5, 7, 9])
+
+    def test_postorder(self):
+        self.assertIsNone(self.tree.postorder())
+        self.tree.insert(5)
+        self.tree.insert(4)
+        self.tree.insert(9)
+        self.tree.insert(7)
+        self.tree.insert(2)
+        self.tree.insert(3)
+        self.assertEqual(self.tree.postorder(), [3, 2, 4, 7, 9, 5])
+    def test_search(self):
+        self.tree.insert(5)
+        self.tree.insert(4)
+        self.tree.insert(9)
+        self.tree.insert(7)
+        self.tree.insert(2)
+        self.tree.insert(3)
+        self.assertTrue(self.tree.search(2))
+        self.assertFalse(self.tree.serach(15))
+    def test_find_max(self):
+        self.tree.insert(5)
+        self.tree.insert(4)
+        self.tree.insert(9)
+        self.tree.insert(7)
+        self.tree.insert(2)
+        self.tree.insert(3)
+        self.assertEqual(self.tree.find_max(), 9)
+    def test_find_min(self):
+        self.tree.insert(5)
+        self.tree.insert(4)
+        self.tree.insert(9)
+        self.tree.insert(7)
+        self.tree.insert(2)
+        self.tree.insert(3)
+        self.assertEqual(self.tree.find_min(), 2)
+
+    def test_delete(self):
+        self.tree.insert(5)
+        self.tree.insert(4)
+        self.tree.insert(9)
+        self.tree.insert(7)
+        self.tree.insert(2)
+        self.tree.insert(3)
+        self.tree.delete(3)
+        self.assertEqual(self.tree.postorder(), [2, 4, 7, 9, 5])
+        self.delete(4)
+        self.assertEqual(self.test_postorder(), [2, 7, 9, 5])
 
 if __name__ == '__main__':
     unittest.main()
