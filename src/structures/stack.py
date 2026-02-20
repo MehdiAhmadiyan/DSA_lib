b1e0c5689ed67a04f4c9b7c6bf86fab22ca73a54
Sina Moradi <sinamoradi002@gmail.com>
2026-02-20 16:13:36 +0330

    stack V.1 completed! OK

--- src/structures/stack.py
+++ src/structures/stack.py
@@ -8,5 +8,23 @@ class stack:
     def push(self, x):
         temp = Node(x)
         temp.next = self.top
-        temp.next.prev = temp
         self.top = temp
+        self.size += 1
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        popped = self.top
+        self.top = popped.next
+        self.size -= 1
+        return popped.data
+
+    def is_empty(self):
+        return self.size == 0
+
+    def get_top(self):
+        if self.is_empty():
+            return None
+        return self.top.data
+
+
--- src/tests/stack_tst.py
+++ src/tests/stack_tst.py
@@ -26,20 +26,20 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.stack.is_empty() , False)
 
     def test_top(self):
-        self.assertEqual(self.stack.top(), None)
+        self.assertEqual(self.stack.get_top(), None)
         self.stack.push(1)
         self.stack.push(2)
-        self.assertEqual(self.stack.top(), 2)
+        self.assertEqual(self.stack.get_top(), 2)
         self.stack.pop()
-        self.assertEqual(self.stack.top(), 1)
+        self.assertEqual(self.stack.get_top(), 1)
 
     def test_pop(self):
-        self.assertEqual(self.stack.top, None)
+        self.assertEqual(self.stack.pop(), None)
         self.stack.push(1)
         self.stack.push(2)
         self.stack.push(3)
         self.stack.pop()
-        self.assertEqual(self.stack.top , 2)
+        self.assertEqual(self.stack.top.data , 2)
         self.assertEqual(self.stack.size, 2)
 
 if __name__ == '__main__':
